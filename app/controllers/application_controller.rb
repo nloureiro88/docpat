f8e1d78dfabd608462747d4a8bd5f49781e6548e
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-03-04 14:16:05 +0000

    Devise setup - only missing email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :user_type])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys:
+                                     [:first_name, :last_name, :photo, :address, :date_birth,
+                                      :identity_card, :pat_blood, :pat_pharma, :pat_pharma_email])
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "Access not authorized."
+    redirect_to(root_path)
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
