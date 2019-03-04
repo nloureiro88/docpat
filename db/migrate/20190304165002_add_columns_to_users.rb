736d5b0cb13b55ff42b33c1d75721efbcfcc386c
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-03-04 16:51:54 +0000

    Added HTTPS and finished users model

---
+++ db/migrate/20190304165002_add_columns_to_users.rb
@@ -0,0 +1,18 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :photo, :string
+    add_column :users, :address, :text
+    add_column :users, :date_birth, :date
+    add_column :users, :identity_card, :integer
+    add_column :users, :user_type, :string, default: "patient"
+    add_column :users, :pat_blood, :string
+    add_column :users, :pat_pharma, :string
+    add_column :users, :pat_pharma_email, :string
+    add_column :users, :doc_number, :integer
+    add_column :users, :doc_institutions, :string
+    add_column :users, :doc_specialties, :string
+    add_column :users, :status, :string, default: "active"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_121944) do
+ActiveRecord::Schema.define(version: 2019_03_04_165002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 2019_03_04_121944) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "photo"
+    t.text "address"
+    t.date "date_birth"
+    t.integer "identity_card"
+    t.string "user_type", default: "patient"
+    t.string "pat_blood"
+    t.string "pat_pharma"
+    t.string "pat_pharma_email"
+    t.integer "doc_number"
+    t.string "doc_institutions"
+    t.string "doc_specialties"
+    t.string "status", default: "active"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
