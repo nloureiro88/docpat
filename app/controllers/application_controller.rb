70712968b2103f378759e4533f0a6b57805b85f2
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-03-08 14:49:40 +0000

    Sidebar finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,6 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-
-
   def after_sign_in_path_for(resource)
     patient_topics_path(current_user)
   end
