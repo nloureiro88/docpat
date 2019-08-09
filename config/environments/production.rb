b5231c416de68699595329a1ac01899642ecdd27
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-08-09 15:39:17 +0100

    Removed SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.force_ssl = true
-  config.action_mailer.default_url_options = { host: "https://www.docpat.pro" }
+  # config.force_ssl = true
+  config.action_mailer.default_url_options = { host: "http://www.docpat.pro" }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
